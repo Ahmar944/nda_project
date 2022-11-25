2dce69193a1a1adc23d9202440e01cc8105aeeff
Ahmar <Ahmarmughal944@gmail.com>
2022-11-25 17:31:29 +0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+<!--     
+    <HelloWorld msg="Welcome to Your Vue.js App"/><br/>
+    <h1 >Register Page: 1</h1>
+    <Register/><br/>
+    <h1>Login Page: 2</h1>
+    <Login/><br/>
+    <h1>Forgot Password: 3</h1>
+    <forgot/><br/>
+    <h1>Verify Email: 4</h1>
+    <Verify/><br/>
+    <h1>Set Password: 5</h1>
+    <Password/><br/>
+    <h1>Updated: 6</h1>
+  
+    <update/> -->
+
+    <main role="main">
+      <router-view></router-view>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import Register from './views/Register.vue'
+// import Login from './views/Login.vue'
+// import Forgot from './views/Forgot.vue'
+// import Verify from './views/Verify.vue'
+// import Password from './views/SetPassword.vue'
+// import Update from './views/Updated.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+    // Register,
+    // Login,
+    // Forgot,
+    // Verify,
+    // Password,
+    // Update
+}
 }
 </script>
 
-<style>
+<style scoped>
+/* .log{
+  text-align: center;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+h1{
+  text-align: center;
+} */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/css/style.css'
+import '../public/assets/Css/bootstrap.min.css'
+import router from './router'
+
+import Vuelidate from 'vuelidate'
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
